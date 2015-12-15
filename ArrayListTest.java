974d5ba88a0345fe428ae6ca47b8516b67949a17
janss001 <janss001@gmail.com>
2015-12-15 22:01:33 +0000

    General improvement of tests

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -2,29 +2,66 @@ public class ArrayListTest {
 	
 	public void launch() {
 		List arrayListTest = new ArrayList();
-		System.out.println(arrayListTest.isEmpty());
-		System.out.println(arrayListTest.size());
-		ReturnObject testResult = arrayListTest.add(null);
-		System.out.println(testResult.getError());
-		arrayListTest.add("First");
-		arrayListTest.add("Second");
-		arrayListTest.add("Third");
-		arrayListTest.add("Fourth");
-		arrayListTest.add("Fifth");
+		ReturnObject testResult;
+		System.out.println("Array initially empty: " + arrayListTest.isEmpty());
+		System.out.print("Attempting to retrieve from empty array...");
 		testResult = arrayListTest.get(0);
-		System.out.println(testResult.getReturnValue());
-		testResult = arrayListTest.get(1);
-		System.out.println(testResult.getReturnValue());
-		testResult = arrayListTest.get(2);
-		System.out.println(testResult.getReturnValue());
-		testResult = arrayListTest.get(3);
-		System.out.println(testResult.getReturnValue());
-		testResult = arrayListTest.get(4);
-		System.out.println(testResult.getReturnValue());
-		testResult = arrayListTest.get(5);
-		System.out.println(testResult.getError());
-		testResult = arrayListTest.get(-1);
-		System.out.println(testResult.getError());
+		printTestResult(testResult);
+		System.out.print("Attempting to add at index to empty array...");
+		testResult = arrayListTest.add(1, "Added0");
+		printTestResult(testResult);
+		System.out.print("Attempting to remove item from empty array...");
+		testResult = arrayListTest.remove(1);
+		printTestResult(testResult);
+		System.out.print("Attempting to add null object to array...");
+		testResult = arrayListTest.add(null);
+		printTestResult(testResult);
+		for (int i = 0; i < 6; i++) {
+			System.out.print("Attempting to add " + i + " to array...");
+			testResult = arrayListTest.add(i);
+			printTestResult(testResult);
+		}
+		System.out.println("Is the array empty?: " + arrayListTest.isEmpty());
+		System.out.println("Number of items in array: " + arrayListTest.size());
+		for (int i = -1; i < 7; i++) {
+			System.out.print("Attempting to retrieve element at index " + i + " from array...");
+			testResult = arrayListTest.get(i);
+			printTestResult(testResult);	
+		}
+		System.out.print("Attempting to add at index 3...");
+		testResult = arrayListTest.add(3, "Added1");
+		printTestResult(testResult);
+		System.out.print("Attempting to add at index 0...");
+		testResult = arrayListTest.add(0, "Added2");
+		printTestResult(testResult);
+		System.out.print("Attempting to add at index 8...");
+		testResult = arrayListTest.add(8, "Added3");
+		printTestResult(testResult);
+		for (int i = 0; i < arrayListTest.size(); i++) {
+			System.out.print("Attempting to retrieve element at index " + i + " from array...");
+			testResult = arrayListTest.get(i);
+			printTestResult(testResult);
+		}
+		System.out.print("Attempting to remove item at index 8...");
+		testResult = arrayListTest.remove(8);
+		printTestResult(testResult);
+		System.out.print("Attempting to remove item at index -1...");
+		testResult = arrayListTest.remove(-1);
+		printTestResult(testResult);
+		for (int i = 7; i >= 0; i--) {
+			System.out.print("Attempting to remove item at index " + i + "...");
+			testResult = arrayListTest.remove(i);
+			printTestResult(testResult);		
+		}
+		System.out.println("Is the array empty?: " + arrayListTest.isEmpty());
+	}
+	
+	public void printTestResult(ReturnObject result) {
+		if (result.hasError()) {
+			System.out.println("Error: " + result.getError());
+		} else {
+			System.out.println("Return Value: " + result.getReturnValue());
+		}
 	}
 	
 	public static void main(String[] args) {
