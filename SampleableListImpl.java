75a9f8678a7baa2cddcf1c29cae269ae8fafabb5
janss001 <janss001@gmail.com>
2015-12-20 12:31:58 +0000

    Initial commit

---
+++ SampleableListImpl.java
@@ -0,0 +1,15 @@
+public class SampleableListImpl extends FunctionalArrayList implements SampleableList {
+	private SampleableList sampleList;
+	
+	@Override
+	/**
+	 * Returns a list containing the first, third, fifth...
+	 * items of this list, or an empty list if the list is empty. 
+	 * 
+	 * @return a list containing the first, third, fifth... items of this list
+	 */
+	public SampleableList sample() {
+		return null;
+	}
+	
+}
