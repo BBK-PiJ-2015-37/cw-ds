21305d3a164942430e70a05c0beb870a1f54d9b4
janss001 <janss001@gmail.com>
2015-12-24 11:00:08 +0000

    First pass at isEmpty(), size(), push(), top() methods

--- StackImpl.java
+++ StackImpl.java
@@ -3,28 +3,41 @@ public class StackImpl extends AbstractStack {
 	public StackImpl(List list) {
 		super(list);
 	}
-
+	
+	@Override
 	/**
 	 * Returns true if the stack is empty, false otherwise. 
 	 * 
 	 * @return true if the stack is empty, false otherwise. 
 	 */
-	public boolean isEmpty();
+	public boolean isEmpty() {
+		return internalList.isEmpty();
+	}
 
+	@Override
 	/**
 	 * Returns the number of items currently in the stack.
 	 * 
 	 * @return the number of items currently in the stack
 	 */
-	public int size();
+	public int size() {
+		return internalList.size();
+	}
 
+	@Override
 	/**
 	 * Adds an element at the top of the stack. 
 	 * 
 	 * @param item the new item to be added
 	 */
-	public void push(Object item);
+	public void push(Object item) {
+		if (item != null) {
+			internalList.add(item);
+		}
+		return;
+	}
 
+	@Override
 	/**
 	 * Returns the element at the top of the stack. The stack is
 	 * left unchanged.
@@ -32,8 +45,15 @@ public class StackImpl extends AbstractStack {
 	 * @return If stack is not empty, the item on the top is returned. If the
 	 *         stack is empty, an appropriate error.
 	 */
-	public ReturnObject top();
+	public ReturnObject top() {
+		if (internalList.isEmpty()) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else {
+			return internalList.get(internalList.size() - 1);
+		}
+	}
 
+	@Override
 	/**
 	 * Returns the element at the top of the stack. The element is
 	 * removed frmo the stack.
@@ -41,5 +61,7 @@ public class StackImpl extends AbstractStack {
 	 * @return If stack is not empty, the item on the top is returned. If the
 	 *         stack is empty, an appropriate error.
 	 */
-	public ReturnObject pop();
+	public ReturnObject pop() {
+		return null;
+	}
 }
