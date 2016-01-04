fcd8488708e9129a7aee07ee3a561ce0c2114c24
janss001 <janss001@gmail.com>
2016-01-04 11:52:30 +0000

    New tests: Pushing large no of elements then popping all, for array and linked lists

--- StackImplTest.java
+++ StackImplTest.java
@@ -10,7 +10,7 @@ public class StackImplTest {
 		//testStack.push("Pushed");
 		//testStack.top();
 		//testStack.pop();
-		System.out.println("Now switching to non-null list...");
+		System.out.println("Now switching to non-null array list...");
 		List testList = new ArrayList();
 		testStack = new StackImpl(testList);
 		ReturnObject testResult;
@@ -48,6 +48,67 @@ public class StackImplTest {
 		System.out.print("Popping top of empty stack...");
 		testResult = testStack.pop();
 		printTestResult(testResult);
+		System.out.print("Adding 1 million items to stack...");
+		for (int i = 0; i < 1000000; i++) {
+			testStack.push("Element " + i);
+		}
+		System.out.println("done");
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("Clearing stack of 1 million items...");
+		while (!testStack.isEmpty()){
+			testStack.pop();
+		}
+		System.out.println("done");
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.println("Now switching to non-null linked list...");
+		testList = new LinkedList();
+		testStack = new StackImpl(testList);
+		System.out.println("Stack initially empty: " + testStack.isEmpty());
+		System.out.println("Size of initial stack: " + testStack.size());
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Pushing " + i + " to stack...");
+			testStack.push(i);
+		}
+		System.out.println("Is stack now empty?: " + testStack.isEmpty());
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("What's top of the stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("Popping top of stack...");
+		testResult = testStack.pop();
+		printTestResult(testResult);
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("What's top of the stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.println("Pushing null to stack...");
+		testStack.push(null);
+		System.out.println("Size of stack: " + testStack.size());
+		for (int i = testStack.size() - 1; i >= 0; i--) {
+			System.out.print("Popping top of stack...");
+			testResult = testStack.pop();
+			printTestResult(testResult);
+		}
+		System.out.println("Is stack now empty?: " + testStack.isEmpty());
+		System.out.print("What's top of the empty stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.print("Popping top of empty stack...");
+		testResult = testStack.pop();
+		printTestResult(testResult);
+		System.out.print("Adding 50,000 items to stack...");
+		for (int i = 0; i < 50000; i++) {
+			testStack.push("Element " + i);
+		}
+		System.out.println("done");
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("Clearing stack of 50,000 items...");
+		while (!testStack.isEmpty()){
+			testStack.pop();
+		}
+		System.out.println("done");
+		System.out.println("Size of stack: " + testStack.size());
 	}
 	
 	public void printTestResult(ReturnObject result) {
