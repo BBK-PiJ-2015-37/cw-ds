7144faa3021152572de33ba18ade8509cf4fec6e
janss001 <janss001@gmail.com>
2016-01-04 12:18:35 +0000

    New tests: Pushing large no of elements then popping all, for array and linked lists

--- ImprovedStackImplTest.java
+++ ImprovedStackImplTest.java
@@ -10,7 +10,7 @@ public class ImprovedStackImplTest {
 		//testStack.push("Pushed");
 		//testStack.top();
 		//testStack.pop();
-		System.out.println("Now switching to non-null list...");
+		System.out.println("Now switching to non-null array list...");
 		List testList = new ArrayList();
 		testStack = new ImprovedStackImpl(testList);
 		ReturnObject testResult;
@@ -80,6 +80,123 @@ public class ImprovedStackImplTest {
 			testResult = testStack.pop();
 			printTestResult(testResult);
 		}
+		System.out.print("Adding 1 million items to stack...");
+		for (int i = 0; i < 1000000; i++) {
+			int randInt = (int)(Math.random() * 1000);
+			testStack.push(randInt);
+		}
+		System.out.println("done");
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.println("Reversing stack...");
+		reversedStack = testStack.reverse();
+		System.out.println("Is reversed stack empty?: " + reversedStack.isEmpty());
+		System.out.println("Size of reversed stack: " + reversedStack.size());
+		System.out.println("Is original stack empty?: " + testStack.isEmpty());
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.print("What's top of the reversed stack? ");
+		testResult = reversedStack.top();
+		printTestResult(testResult);
+		System.out.print("What's top of the original stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		int randInt = (int)(Math.random() * 1000);
+		System.out.println("Removing " + randInt + " from reversed stack...");
+		reversedStack.remove(randInt);
+		System.out.println("Size of reversed stack: " + reversedStack.size());
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.println("Now switching to non-null linked list...");
+		testList = new LinkedList();
+		testStack = new ImprovedStackImpl(testList);
+		System.out.println("Stack initially empty: " + testStack.isEmpty());
+		System.out.println("Size of initial stack: " + testStack.size());
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Pushing " + i + " to stack...");
+			testStack.push(i);
+		}
+		System.out.println("Is stack now empty?: " + testStack.isEmpty());
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("What's top of the stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("Popping top of stack...");
+		testResult = testStack.pop();
+		printTestResult(testResult);
+		System.out.println("Size of stack: " + testStack.size());
+		System.out.print("What's top of the stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.println("Pushing null to stack...");
+		testStack.push(null);
+		System.out.println("Size of stack: " + testStack.size());
+		for (int i = testStack.size() - 1; i >= 0; i--) {
+			System.out.print("Popping top of stack...");
+			testResult = testStack.pop();
+			printTestResult(testResult);
+		}
+		System.out.println("Is stack now empty?: " + testStack.isEmpty());
+		System.out.print("What's top of the empty stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		System.out.print("Popping top of empty stack...");
+		testResult = testStack.pop();
+		printTestResult(testResult);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Pushing " + i + " to stack...");
+			testStack.push(i);
+		}
+		System.out.println("Reversing stack...");
+		reversedStack = testStack.reverse();
+		System.out.println("Is reversed stack empty?: " + reversedStack.isEmpty());
+		System.out.println("Size of reversed stack: " + reversedStack.size());
+		System.out.println("Is original stack empty?: " + testStack.isEmpty());
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.print("What's top of the reversed stack? ");
+		testResult = reversedStack.top();
+		printTestResult(testResult);
+		for (int i = reversedStack.size() - 1; i >= 0; i--) {
+			System.out.print("Popping top of reversed stack...");
+			testResult = reversedStack.pop();
+			printTestResult(testResult);
+		}
+		System.out.println("Removing 2 from original stack...");
+		testStack.remove(2);
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.println("Removing 6 from original stack...");
+		testStack.remove(6);
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.println("Removing 'Hello' from original stack...");
+		testStack.remove("Hello");
+		System.out.println("Size of original stack: " + testStack.size());
+		for (int i = testStack.size() - 1; i >= 0; i--) {
+			System.out.print("Popping top of original stack...");
+			testResult = testStack.pop();
+			printTestResult(testResult);
+		}
+		System.out.print("Adding 50,000 items to stack...");
+		for (int i = 0; i < 50000; i++) {
+			randInt = (int)(Math.random() * 1000);
+			testStack.push(randInt);
+		}
+		System.out.println("done");
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.println("Reversing stack...");
+		reversedStack = testStack.reverse();
+		System.out.println("Is reversed stack empty?: " + reversedStack.isEmpty());
+		System.out.println("Size of reversed stack: " + reversedStack.size());
+		System.out.println("Is original stack empty?: " + testStack.isEmpty());
+		System.out.println("Size of original stack: " + testStack.size());
+		System.out.print("What's top of the reversed stack? ");
+		testResult = reversedStack.top();
+		printTestResult(testResult);
+		System.out.print("What's top of the original stack? ");
+		testResult = testStack.top();
+		printTestResult(testResult);
+		randInt = (int)(Math.random() * 1000);
+		System.out.println("Removing " + randInt + " from reversed stack...");
+		reversedStack.remove(randInt);
+		System.out.println("Size of reversed stack: " + reversedStack.size());
+		System.out.println("Size of original stack: " + testStack.size());
 	}
 	
 	public void printTestResult(ReturnObject result) {
